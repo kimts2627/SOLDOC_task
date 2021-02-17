eaa03b77632c4d9680a32dae3d909b0f51d21a16
kimts2627 <kimtstststs@gmail.com>
2021-02-17 23:25:09 +0900

    Edit Header style

--- src/components/atoms/SoldocLogo.jsx
+++ src/components/atoms/SoldocLogo.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 const SoldocLogo = () => {
   return (
-    <h3 className="Title sub">
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9.26213 0.0699158H0V5.89199H10.0963V16H15.9032V6.75781C12.9645 5.40804 10.5957 3.02335 9.26213 0.0699158Z" fill="#007CBD"/>
+    <div>
+      <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.9012 4.0498V0H10.0943V4.55609C10.0943 10.8469 15.1809 15.9468 21.4553 15.9468H26V10.1248H21.9608C18.6139 10.1248 15.9012 7.40492 15.9012 4.0498Z" fill="#EE7F3B"/>
+        <path d="M9.26213 10.0699H0V15.892H10.0963V26H15.9032V16.7578C12.9645 15.408 10.5957 13.0234 9.26213 10.0699Z" fill="#007CBD"/>
       </svg>
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5.90114 4.0498V0H0.0942688V4.55609C0.0942688 10.8469 5.18092 15.9468 11.4553 15.9468H16V10.1248H11.9608C8.61389 10.1248 5.90114 7.40492 5.90114 4.0498Z" fill="#EE7F3B"/>
-      </svg>
-
-    </h3>
+    </div>
   );
 }
 
