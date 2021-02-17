085ac716c3bd0c9c314818a32ed7d04e87542cc7
kimts2627 <kimtstststs@gmail.com>
2021-02-18 00:17:06 +0900

    Edit Date, Time, Footer style

--- src/components/atoms/InputBox.jsx
+++ src/components/atoms/InputBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const InputBox = ({ inputType }) => {
   return (
     <input
-      type='text'
+      type='textarea'
       placeholder={
         inputType === 'additional' ? '평소 앓고 있던 질병, 또는 처방전 솔닥 닥터가 알아야 할 내용이 있다면 여기에 적어주세요.' : undefined
       }
