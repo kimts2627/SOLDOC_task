8f30256c7b59ab68f43fb29d5a8b3b21b7b6e797
kimts2627 <kimtstststs@gmail.com>
2021-02-17 22:47:28 +0900

    Add inputbox atom

---
+++ src/components/atoms/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputBox = ({ inputType }) => {
+  return (
+    <input type='text' className="input">
+      진료 예약하기
+    </input>
+  );
+}
+
+export default InputBox;
